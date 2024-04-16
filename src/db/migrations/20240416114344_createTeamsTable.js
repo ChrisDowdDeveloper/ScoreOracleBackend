aed530bf1b91e1125f72d4da653e320e26ea8445
Chris Dowd <chrisdowd1996@gmail.com>
2024-04-16 10:49:51 -0500

    teams schema created

--- src/db/migrations/20240416114344_createTeamsTable.js
+++ src/db/migrations/20240416114344_createTeamsTable.js
@@ -3,7 +3,13 @@
  * @returns { Promise<void> }
  */
 exports.up = function(knex) {
-  
+  return knex.schema.createTable("teams", (table) => {
+    table.increments("team_id").unsigned().notNullable();
+    table.string("team_name");
+    table.string("sport_type");
+    table.string("league");
+    table.string("logo_url");
+  })
 };
 
 /**
@@ -11,5 +17,5 @@ exports.up = function(knex) {
  * @returns { Promise<void> }
  */
 exports.down = function(knex) {
-  
+  return knex.schema.dropTable("teams");
 };
