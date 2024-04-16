74d53e82cb583f8a677d37b907a0673aefb0f720
Chris Dowd <chrisdowd1996@gmail.com>
2024-04-16 08:34:25 -0500

    matches migration done

--- src/db/migrations/20240416114353_createMatchesTable.js
+++ src/db/migrations/20240416114353_createMatchesTable.js
@@ -3,13 +3,35 @@
  * @returns { Promise<void> }
  */
 exports.up = function(knex) {
+    return knex.schema.createTable("matches", (table) => {
+      table.increments("match_id").unsigned().notNullable();
+      table.integer("user_id").unsigned().notNullable();
+      table.foreign("user_id")
+          .references("user_id")
+          .inTable("users")
+          .onDelete("CASCADE");
+      table.timestamp("match_date");
+      table.string("league");
+      table.integer("home_team").unsigned().notNullable();
+      table.foreign("home_team")
+          .references("team_id")
+          .inTable("teams")
+          .onDelete("CASCADE");
+      table.integer("away_team").unsigned().notNullable();
+      table.foreign("away_team")
+          .references("team_id")
+          .inTable("teams")
+          .onDelete("CASCADE");
+      table.string("location");
+      table.string("status").notNullable().defaultTo("upcoming");
+    });
+  };
   
-};
-
-/**
- * @param { import("knex").Knex } knex
- * @returns { Promise<void> }
- */
-exports.down = function(knex) {
+  /**
+   * @param { import("knex").Knex } knex
+   * @returns { Promise<void> }
+   */
+  exports.down = function(knex) {
+    return knex.schema.dropTable("matches");
+  };
   
-};
