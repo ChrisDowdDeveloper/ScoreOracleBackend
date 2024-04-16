cbf12d8bd587a1af716c0f0f4bf05cada73e3fd3
Chris Dowd <chrisdowd1996@gmail.com>
2024-04-16 10:57:04 -0500

    changes to the schema

--- src/db/migrations/20240416114344_createTeamsTable.js
+++ src/db/migrations/20240416114344_createTeamsTable.js
@@ -3,19 +3,20 @@
  * @returns { Promise<void> }
  */
 exports.up = function(knex) {
-  return knex.schema.createTable("teams", (table) => {
-    table.increments("team_id").unsigned().notNullable();
-    table.string("team_name");
-    table.string("sport_type");
-    table.string("league");
-    table.string("logo_url");
-  })
-};
-
-/**
- * @param { import("knex").Knex } knex
- * @returns { Promise<void> }
- */
-exports.down = function(knex) {
-  return knex.schema.dropTable("teams");
-};
+    return knex.schema.createTable("teams", (table) => {
+      table.increments("team_id").unsigned().notNullable();
+      table.string("team_name").notNullable();
+      table.string("sport_type").notNullable();
+      table.string("league").notNullable(); 
+      table.string("logo_url"); 
+    });
+  };
+  
+  /**
+   * @param { import("knex").Knex } knex
+   * @returns { Promise<void> }
+   */
+  exports.down = function(knex) {
+    return knex.schema.dropTable("teams");
+  };
+  
