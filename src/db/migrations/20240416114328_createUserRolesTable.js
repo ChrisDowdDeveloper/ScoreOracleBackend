498257d6f303a275f51c29faf79aa33b047dfa6b
Chris Dowd <chrisdowd1996@gmail.com>
2024-04-16 11:02:19 -0500

    user roles schema and migration created

--- src/db/migrations/20240416114328_createUserRolesTable.js
+++ src/db/migrations/20240416114328_createUserRolesTable.js
@@ -3,7 +3,15 @@
  * @returns { Promise<void> }
  */
 exports.up = function(knex) {
-  
+  return knex.schema.createTable("user_roles", (table) => {
+    table.increments("user_role_id").unsigned().notNullable();
+    table.integer("user_id").unsigned().notNullable();
+    table.foreign("user_id")
+        .references("user_id")
+        .inTable("users")
+        .onDelete("CASCADE");
+    table.string("role").notNullable().defaultTo("user");
+  })
 };
 
 /**
@@ -11,5 +19,5 @@ exports.up = function(knex) {
  * @returns { Promise<void> }
  */
 exports.down = function(knex) {
-  
+  return knex.schema.dropTable("user_roles");
 };
