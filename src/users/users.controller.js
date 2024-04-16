c11330c3347c349edff40ec04eb24685cb73a7d7
Chris Dowd <chrisdowd1996@gmail.com>
2024-04-16 11:46:05 -0500

    users can successfully be created, updated, listed, and deleted

--- src/users/users.controller.js
+++ src/users/users.controller.js
@@ -1,85 +1,6 @@
 const service = require("./users.service");
 const asyncErrorBoundary = require("../errors/asyncErrorBoundary");
 
-const VALID_FIELDS = [
-    "username",
-    "email",
-    "password",
-    "firstName",
-    "lastName",
-    "dateJoined",
-    "dateOfBirth",
-    "profilePictureURL"
-];
-
-function dateOfBirthValid(req, res, next) {
-    const { dateOfBirth } = req.body.data;
-    const day = new Date(dateOfBirth).getUTCDate();
-    const today = new Date();
-    if(day > today) {
-        return next({
-            status: 400,
-            message: "Date of birth cannot be greater than today",
-        });
-    } else {
-        return next();
-    }
-}
-
-function formHasInput(req, res, next) {
-    const { data = {} } = req.body;
-    try {
-        VALID_FIELDS.forEach((fields) => {
-            if(!data[fields]) {
-                const error = new Error(`A '${fields} is required`)
-                error.status = 400;
-                throw error;
-            }
-        });
-        next();
-    } catch(err) {
-        next(err);
-    }
-}
-
-async function userExists(req, res, next) {
-    const { user_id } = req.body;
-    const userById = await service.read(user_id);
-    if(userById) {
-        res.locals.user = userById;
-        return next();
-    }
-    next({
-        status: 404,
-        message: "User not found"
-    });
-}
-
-function usernameIsValid(req, res, next) {
-    const username = req.body.data.username;
-    const usernameFound = service.read(username);
-    if(!usernameFound) {
-        return next();
-    }
-    return next({
-        status: 400,
-        message: "Username is already taken"
-    });
-}
-
-function emailIsValid(req, res, next) {
-    const regexPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    const email = req.body.data.user_email;
-    if(regexPattern.test(email)) {
-        return next();
-    }
-    return next({
-        status: 400,
-        message: "Email must be in the format of example@example.com"
-    })
-}
-
-
 async function list(req, res) {
     const { username } = req.query;
     if(username) {
@@ -89,8 +10,7 @@ async function list(req, res) {
 }
 
 async function create(req, res) {
-    const { data } = req.body;
-    const created = await service.create(data);
+    const created = await service.create(req.body);
     res.status(201).json({ data: created });
 }
 
@@ -103,14 +23,14 @@ async function read(req, res) {
 
 async function update(req, res, next) {
     const { user_id } = req.params;
-    const updated = { ...req.body.data };
+    const updated = { ...req.body };
     const data = await service.update(updated, user_id);
     res.status(200).json({ data });
 }
 
 async function deleteUser(req, res, next) {
     const { user_id } = req.params;
-    const deletedUser = await service.delete(user_id);
+    const deletedUser = await service.deleteUser(user_id);
     if(deletedUser) {
         res.json({ message: "user successfully deleted" })
     }
@@ -118,21 +38,8 @@ async function deleteUser(req, res, next) {
 
 module.exports = {
     list: asyncErrorBoundary(list),
-    create: [
-        formHasInput,
-        dateOfBirthValid,
-        usernameIsValid,
-        emailIsValid,
-        asyncErrorBoundary(create),
-    ],
-    read: [asyncErrorBoundary(userExists), asyncErrorBoundary(read)],
-    update: [
-        asyncErrorBoundary(userExists),
-        formHasInput,
-        dateOfBirthValid,
-        usernameIsValid,
-        emailIsValid,
-        asyncErrorBoundary(update)
-    ],
+    create: asyncErrorBoundary(create),
+    read: asyncErrorBoundary(read),
+    update: asyncErrorBoundary(update),
     delete: asyncErrorBoundary(deleteUser)
 };
