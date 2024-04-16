7002ac35ba1bbff8c2561b0692b7e32737f87c5c
Chris Dowd <chrisdowd1996@gmail.com>
2024-04-16 11:11:09 -0500

    user schema created

--- src/db/migrations/20240416114312_createUsersTable.js
+++ src/db/migrations/20240416114312_createUsersTable.js
@@ -1,9 +1,19 @@
+import { userIcon } from "../../resources";
 /**
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
 exports.up = function(knex) {
-  
+  return knex.schema.createTable("users", (table) => {
+    table.increments("user_id").unsigned().notNullable();
+    table.string("username").unique().notNullable();
+    table.string("email").unique().notNullable();
+    table.string("password").notNullable();
+    table.string("first_name").notNullable();
+    table.string("last_name").notNullable();
+    table.timestamp("date_joined").defaultTo(knex.fn.now());
+    table.string("profile_picture_url").defaultTo(userIcon);
+  })
 };
 
 /**
@@ -11,5 +21,5 @@ exports.up = function(knex) {
  * @returns { Promise<void> }
  */
 exports.down = function(knex) {
-  
+  return knex.schema.dropTable("users");
 };
